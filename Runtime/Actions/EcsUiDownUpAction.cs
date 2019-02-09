9e97445aca760ea0f1a53acb1a887065a3a9b23e
Leopotam <leopotam@gmail.com>
2019-02-09 23:45:22 +0300

    * Actions: EcsUiDownUpAction - down / up events support. * Components: EcsUiDownEvent / EcsUiDownEvent - down / up events data support.

---
+++ Runtime/Actions/EcsUiDownUpAction.cs
@@ -0,0 +1,34 @@
+﻿// ----------------------------------------------------------------------------
+// The MIT License
+// Ui extension https://github.com/Leopotam/ecs-ui
+// for ECS framework https://github.com/Leopotam/ecs
+// Copyright (c) 2017-2019 Leopotam <leopotam@gmail.com>
+// ----------------------------------------------------------------------------
+
+using Leopotam.Ecs.Ui.Components;
+using UnityEngine.EventSystems;
+
+namespace Leopotam.Ecs.Ui.Actions {
+    /// <summary>
+    /// Ui action for processing down / up input events.
+    /// </summary>
+    public sealed class EcsUiDownUpAction : EcsUiActionBase, IPointerDownHandler, IPointerUpHandler {
+        void IPointerDownHandler.OnPointerDown (PointerEventData eventData) {
+            if ((object) Emitter != null) {
+                var msg = Emitter.CreateMessage<EcsUiDownEvent> ();
+                msg.WidgetName = WidgetName;
+                msg.Sender = gameObject;
+                msg.Position = eventData.position;
+            }
+        }
+
+        void IPointerUpHandler.OnPointerUp (PointerEventData eventData) {
+            if ((object) Emitter != null) {
+                var msg = Emitter.CreateMessage<EcsUiUpEvent> ();
+                msg.WidgetName = WidgetName;
+                msg.Sender = gameObject;
+                msg.Position = eventData.position;
+            }
+        }
+    }
+}
---
+++ Runtime/Components/EcsUiDownEvent.cs
@@ -0,0 +1,23 @@
+// ----------------------------------------------------------------------------
+// The MIT License
+// Ui extension https://github.com/Leopotam/ecs-ui
+// for ECS framework https://github.com/Leopotam/ecs
+// Copyright (c) 2017-2019 Leopotam <leopotam@gmail.com>
+// ----------------------------------------------------------------------------
+
+using UnityEngine;
+
+namespace Leopotam.Ecs.Ui.Components {
+    [EcsOneFrame]
+    public sealed class EcsUiDownEvent : IEcsAutoResetComponent {
+        public string WidgetName;
+
+        public GameObject Sender;
+
+        public Vector2 Position;
+
+        void IEcsAutoResetComponent.Reset () {
+            Sender = null;
+        }
+    }
+}
